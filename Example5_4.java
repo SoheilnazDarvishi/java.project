eeacb37b5c9764ea99d680618bb4e7038b3cfa1e
SoheilnazDarvishi <darvishi.so94@yahoo.com>
2024-07-02 11:18:20 +0330

    Update Example5_4.java

--- Example5_4.java
+++ Example5_4.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class StringComparisonExample {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("Enter the first string: ");
+        String str1 = console.next();
+
+        System.out.print("Enter the second string: ");
+        String str2 = console.next();
+
+        int comparisonResult = str1.compareTo(str2);
+
+        if (comparisonResult == 0) {
+            System.out.println("The strings are equal.");
+        } else if (comparisonResult < 0) {
+            System.out.println("The first string comes before the second string.");
+        } else {
+            System.out.println("The first string comes after the second string.");
+        }
+    }
+}
