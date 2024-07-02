e74ebc3335a5ffc76ca297213fb1c86924fa58a7
SoheilnazDarvishi <darvishi.so94@yahoo.com>
2024-07-02 11:15:46 +0330

    Update Example5_3.java

--- Example5_3.java
+++ Example5_3.java
@@ -1,6 +1,25 @@
 public class Example5_3
 public static void main(String[] args){
-String str="conputer science is a dynamic science.";
-  int count;
-  count= countSpaces(str);
-  
+   String str = "Computer science is a dynamic science.";
+        String substr = "science";
+
+        int spaceCount = countSpaces(str);
+        System.out.println("String is: " + str);
+        System.out.println("Number of spaces is: " + spaceCount);
+
+        int substringOccurrences = substringCount(str, substr);
+        System.out.println("***");
+        System.out.println("String is: " + str);
+        System.out.println("Substring is: " + substr);
+        System.out.println("Substring occurs " + substringOccurrences + " times in str.");
+    }
+
+    public static int countSpaces(String s) {
+        int count = 0;
+        int index = s.indexOf(' ');
+        while (index >= 0) {
+            count++;
+            index = s.indexOf(' ', index + 1);
+        }
+        return count;
+    }
